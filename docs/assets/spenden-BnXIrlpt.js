e2a816c03534804e84ae3db75fac5626fd75312c
Kim Schregenberger <107576899+kim-schregenberger@users.noreply.github.com>
2025-01-29 20:55:35 +0100

    Deploy to GitHub Pages

---
+++ docs/assets/spenden-BnXIrlpt.js
@@ -0,0 +1,2 @@
+import"./animation-CSo1E1N-.js";import{E as e}from"./Experience-k44GAHeM.js";new e(document.querySelector("canvas.webgl"));
+//# sourceMappingURL=spenden-BnXIrlpt.js.map
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import restart from 'vite-plugin-restart'
 import glsl from 'vite-plugin-glsl'
 
 export default {
+    base: '/mp-test/',
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    // base: './',
     server:
     {
         host: true, // Open to local network and display URL
@@ -11,9 +13,17 @@ export default {
     },
     build:
     {
-        outDir: '../dist', // Output in the dist/ folder
+        outDir: '../docs', // Output in the dist/ folder
         emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+        sourcemap: true, // Add sourcemap
+        rollupOptions: {
+            input: {
+                main: 'src/index.html',
+                erfahrungen: 'src/erfahrungen.html',
+                info: 'src/info.html',
+                spenden: 'src/spenden.html',
+            },
+        },
     },
     plugins:
     [
